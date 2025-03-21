c50b6eb1979f6314ab8fdce3cec970a1e593b362
zuna <tales.of.zuna@gmail.com>
2025-03-21 21:38:58 +0800

    +

--- components/toggles/teams.tsx
+++ components/toggles/teams.tsx
@@ -36,75 +36,85 @@ const Teams = ({ teamsWithPlayers, observedPlayer, teamScores }: any) => {
         </div>
       </div>
       <div className="z-10 h-full w-full">
-        {teamsWithPlayers?.map((team: any, index: number) => (
-          <AnimatePresence key={index}>
-            <motion.div
-              initial={{ opacity: 0, x: 50 }}
-              animate={{ opacity: 1, x: 0 }}
-              transition={{ duration: 0.5, delay: index * 0.1 }}
-              exit={{ opacity: 0, x: 50 }}
-              className={`flex h-[43.5px] w-full ${
-                team?.players?.some(
-                  (player: any) => player.isOutsideBlueCircle === true,
-                ) && team?.players?.some((player: any) => player.health > 0)
-                  ? "z-30 animate-pulse bg-blue-700 bg-opacity-30 backdrop-blur-lg"
-                  : ""
-              } text-xl font-bold ${
-                team?.players?.every((player: any) => player.health <= 0)
-                  ? "bg-black bg-opacity-90 text-white grayscale backdrop-blur-sm"
-                  : "text-neutral-800"
-              }`}
-            >
-              <div
-                className={`flex ${
+        {teamsWithPlayers
+          ?.map((team: any) => {
+            const teamScore =
+              teamScores?.find(
+                (score: any) => score.teamName === team?.teamName,
+              )?.totalPoints || 0;
+            return {
+              ...team,
+              totalScore: teamScore + team.teamKillNum,
+            };
+          })
+          .sort((a: any, b: any) => b.totalScore - a.totalScore)
+          .map((team: any, index: number) => (
+            <AnimatePresence key={index}>
+              <motion.div
+                initial={{ opacity: 0, x: 50 }}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{ duration: 0.5, delay: index * 0.1 }}
+                exit={{ opacity: 0, x: 50 }}
+                className={`flex h-[43.5px] w-full ${
                   team?.players?.some(
-                    (player: any) => player.uId == observedPlayer,
-                  )
-                    ? "border-l-5 border-[#FEE75C]"
+                    (player: any) => player.isOutsideBlueCircle === true,
+                  ) && team?.players?.some((player: any) => player.health > 0)
+                    ? "z-30 animate-pulse bg-blue-700 bg-opacity-30 backdrop-blur-lg"
                     : ""
-                } h-full w-[40px] items-center justify-center overflow-hidden text-white`}
+                } text-xl font-bold ${
+                  team?.players?.every((player: any) => player.health <= 0)
+                    ? "bg-black bg-opacity-90 text-white grayscale backdrop-blur-sm"
+                    : "text-neutral-800"
+                }`}
               >
-                {index + 1}
-              </div>
-              <div className="relative flex h-full w-[140px] items-center justify-start text-medium">
-                <div className="flex w-[70px] justify-center">
-                  <Image
-                    src={`/assets/images/teams/${team?.teamName}.png`}
-                    alt=""
-                    className="aspect-square h-[30px]"
-                  />
+                <div
+                  className={`flex ${
+                    team?.players?.some(
+                      (player: any) => player.uId == observedPlayer,
+                    )
+                      ? "border-l-5 border-[#FEE75C]"
+                      : ""
+                  } h-full w-[40px] items-center justify-center overflow-hidden text-white`}
+                >
+                  {index + 1}
                 </div>
-                {team?.teamName}
-              </div>
+                <div className="relative flex h-full w-[140px] items-center justify-start text-medium">
+                  <div className="flex w-[70px] justify-center">
+                    <Image
+                      src={`/assets/images/teams/${team?.teamName}.png`}
+                      alt=""
+                      className="aspect-square h-[30px]"
+                    />
+                  </div>
+                  {team?.teamName}
+                </div>
 
-              <div className="flex h-full w-[85px] items-end justify-center gap-1 py-2">
-                {team?.players?.map((player: any) => {
-                  return (
-                    <div
-                      className={`flex h-full items-end bg-neutral-700`}
-                      key={player?.uId}
-                    >
+                <div className="flex h-full w-[85px] items-end justify-center gap-1 py-2">
+                  {team?.players?.map((player: any) => {
+                    return (
                       <div
-                        className={`w-[6px] ${player.liveState == 4 ? "bg-[#fb8500]" : "bg-green-600"}`}
-                        style={{
-                          height: `${(player?.health / player?.healthMax) * 100}%`,
-                        }}
-                      ></div>
-                    </div>
-                  );
-                })}
-              </div>
-              <div className="flex h-full w-[70px] items-center justify-center text-white">
-                {(teamScores?.find(
-                  (score: any) => score.teamName === team?.teamName,
-                )?.totalPoints || 0) + team.teamKillNum}
-              </div>
-              <div className="flex h-full w-[70px] items-center justify-center text-white">
-                {team?.teamKillNum}
-              </div>
-            </motion.div>
-          </AnimatePresence>
-        ))}
+                        className={`flex h-full items-end bg-neutral-700`}
+                        key={player?.uId}
+                      >
+                        <div
+                          className={`w-[6px] ${player.liveState == 4 ? "bg-[#fb8500]" : "bg-green-600"}`}
+                          style={{
+                            height: `${(player?.health / player?.healthMax) * 100}%`,
+                          }}
+                        ></div>
+                      </div>
+                    );
+                  })}
+                </div>
+                <div className="flex h-full w-[70px] items-center justify-center text-white">
+                  {team.totalScore}
+                </div>
+                <div className="flex h-full w-[70px] items-center justify-center text-white">
+                  {team?.teamKillNum}
+                </div>
+              </motion.div>
+            </AnimatePresence>
+          ))}
       </div>
       <div className="absolute bottom-9 flex h-[40px] w-full items-center justify-around gap-2 bg-[#030e22] px-8 text-lg font-bold">
         <div className="flex items-center gap-1">
